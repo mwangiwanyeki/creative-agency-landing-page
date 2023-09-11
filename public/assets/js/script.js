7bf8384ad53ac26204d79c2fb9e579700aaade28
unknown <mwangiwanyeki@gmail.com>
2023-09-11 12:57:15 +0300

    creative commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', ()=>{
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+const headerMenu = document.querySelector(".header__menu"),
+menuBtn = document.querySelector(".menu-btn"),
+headerMenuItems = document.querySelectorAll("li a");
+
+menuBtn.addEventListener("click", ()=>{
+    headerMenu.classList.toggle("show")
+})
+
+headerMenuItems.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        headerMenu.classList.remove("show")
+    });
+});
